a97bcfcf72fa8bbd9ba500a898e3b30092fe482e
Danielle Eng <dhe61359@creighton.edu>
2021-02-28 18:59:01 -0600

    array

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -5,16 +5,17 @@ let state = prompt("What state do you live in (2 letters, capitalized NE or FL)?
 
 let temp = prompt("What is the current outside temperature in Fahrenheit?")
 
-let messages = [firstName, state, temp]
+let messages = ["wear a warm coat, hat, scarf, and gloves.", "wear a warm coat, but you won't need a hat, scarf, or gloves.", "wear your warmest coat, a warm hat, scarf, and warm gloves.", "wear a warm coat, hat, and gloves. Maybe a scarf too."]
+
 
 //messages to output based on the state and temperature received from the user using if/else statements. There are 4 outcomes: 
 if (state === 'NE' && temp < 32)
-    console.log(`${firstName}, wear a warm coat, hat, scarf, and gloves.`)
+    console.log(`${firstName}, ${messages[0]}`)
 else if (state === 'NE' && temp >= 32 && temp <= 50)
-    console.log(`${firstName}, wear a warm coat, but you won't need a hat, scarf, or gloves.`)
+    console.log(`${firstName}, ${messages[1]}`)
 else if (state === 'FL' && temp >= 32 && temp <= 50)
-    console.log(`${firstName}, wear your warmest coat, a warm hat, scarf, and warm gloves.`)
+    console.log(`${firstName}, ${messages[2]}`)
 else if (state === 'FL' && temp >= 50 && temp <= 70)
-    console.log(`${firstName}, wear a warm coat, hat, and gloves. Maybe a scarf too.`)
+    console.log(`${firstName}, ${messages[3]}`)
 else 
     console.log(`Unable to give recommendation.`)
--- forms/switch/switch.js
+++ forms/switch/switch.js
@@ -5,23 +5,23 @@ let state2 = prompt("What state do you live in (2 letters, capitalized NE or FL)
 
 let temp2 = prompt("What is the current outside temperature in Fahrenheit?")
 
-let messages2 = [firstName2, state2, temp2]
+let messages2 = ["wear a warm coat, hat, scarf, and gloves.", "wear a warm coat, but you won't need a hat, scarf, or gloves.", "wear your warmest coat, a warm hat, scarf, and warm gloves.", "wear a warm coat, hat, and gloves. Maybe a scarf too."]
 
 
 
 //messages to output based on the state and temperature received from the user using switch statements
 switch (true) {
 case (state2 == "NE") && (temp2 < 32):
-    console.log(`${firstName2}, wear a warm coat, hat, scarf, and gloves.`)
+    console.log(`${firstName2}, ${messages2[0]}`)
     break
 case (state2 == "NE") && (temp2 >= 32) && (temp2 <= 50):
-    console.log(`${firstName2}, wear a warm coat, but you won't need a hat, scarf, or gloves.`)
+    console.log(`${firstName2}, ${messages2[1]}`)
     break
 case (state2 == "FL") && (temp2 >= 32) && (temp2 <= 50):
-    console.log(`${firstName2}, wear your warmest coat, a warm hat, scarf, and warm gloves.`)
+    console.log(`${firstName2}, ${messages2[2]}`)
     break
 case (state2 == "FL") && (temp2 >= 50) && (temp2 <= 70):
-    console.log(`${firstName2}, wear a warm coat, hat, and gloves. Maybe a scarf too.`)
+    console.log(`${firstName2}, ${messages2[3]}`)
     break
 default: 
     console.log(`Unable to give recommendation.`)
